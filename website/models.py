c4ffa6ac374978b78be5173da1fd92d555143c2d
ehoang0106 <khoahoang1698@gmail.com>
2023-10-08 02:10:15 -0700

    update models.py and reset-password.html

--- website/models.py
+++ website/models.py
@@ -1,18 +1,20 @@
 from . import db #import database
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
+from datetime import datetime
 import pytz
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000)) #this is a note user can input
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=datetime.now(pytz.timezone('US/Pacific')))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #assign the note to correct user_id
 
-    def formatted_date(self, ):
-        
-        return self.date.strftime('%Y-%m-%d | %I:%M %p')
+    def formatted_date(self):
+        pacific_timezone = pytz.timezone('US/Pacific')
+        pacific_time = self.date.astimezone(pacific_timezone)
+
+        return pacific_time.strftime('%Y-%m-%d | %I:%M %p')
 
 
 class User(db.Model, UserMixin):
